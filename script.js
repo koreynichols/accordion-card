29e48b628c799b25f96882df7724b8c49fb4928f
Korey Nichols <koreynichols1@gmail.com>
2021-10-28 07:46:23 -0700

    have answers toggling when question is clicked on

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.querySelector('.question').addEventListener("click", showAnswer);
-function showAnswer() {
-    this.nextElementSibling.classList.remove("hidden");
+questions = document.querySelectorAll('.question')
+
+questions.forEach(element => {
+    element.addEventListener("click", toggleAnswer);
+});
+
+function toggleAnswer() {
+    isHidden = checkIfHidden(this.nextElementSibling.classList);
+    if(isHidden === true){
+        this.nextElementSibling.classList.remove("hidden");
+        return;
+    }
+    this.nextElementSibling.classList.add("hidden");
+}
+
+function checkIfHidden(list_of_classes) {
+    for(i=0; i<list_of_classes.length; i++){
+        if(list_of_classes[i] === 'hidden')
+            return true;
+    }
+    return false
 }
